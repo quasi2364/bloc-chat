4d9e8cce4f30b42809d1ed586baded5390c94536
Dan Levin <dlevin64@gmail.com>
2016-06-12 11:17:27 -0400

    story 4 part way

--- dist/scripts/services/Rooms.js
+++ dist/scripts/services/Rooms.js
@@ -2,13 +2,23 @@
 	function Rooms ($firebaseArray) {
 		var Rooms = {};
 		var ref = new Firebase("https://dansblocchat.firebaseio.com/");
-		
+		Rooms.messages = $firebaseArray(ref.child('messages'));
+
+
 		Rooms.all = $firebaseArray(ref.child('rooms'));
 
 		Rooms.addRoom = function(room) {
 			Rooms.all.$add(room);
 		};
 
+		// Rooms.getMessages = function() {
+		// 	messages.$loaded().then(function(data) {
+		// 		var x = data.orderByChild('roomId').equalTo('-KJwxt6VJ25O6dN1UFxH');
+		// 		console.log(x);
+		// 		return x;
+		// 	});
+		// };
+			
 		return Rooms;
 	}
 
